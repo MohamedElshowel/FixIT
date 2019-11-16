273bbe8aff07e1d12eaed3be918199f1bbee8b8a
Ahmed Hussein <ahmedhussein.developer@gmail.com>
2019-11-16 20:47:42 +0200

    init

---
+++ mobile/repairit/navigation/MainTabNavigator.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import { Platform } from 'react-native'
+import {
+  createStackNavigator,
+  createBottomTabNavigator
+} from 'react-navigation'
+
+import TabBarIcon from '../components/TabBarIcon'
+import HomeScreen from '../screens/HomeScreen'
+import ProfileScreen from '../screens/ProfileScreen'
+
+const config = Platform.select({
+  web: { headerMode: 'screen' },
+  default: {}
+})
+
+const HomeStack = createStackNavigator(
+  {
+    Home: HomeScreen
+  },
+  config
+)
+
+HomeStack.navigationOptions = {
+  tabBarLabel: 'Home',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={
+        Platform.OS === 'ios'
+          ? `ios-book${focused ? '' : '-outline'}`
+          : 'md-book'
+      }
+    />
+  )
+}
+
+HomeStack.path = ''
+
+const ProfileStack = createStackNavigator(
+  {
+    Links: ProfileScreen
+  },
+  config
+)
+
+ProfileStack.navigationOptions = {
+  tabBarLabel: 'Profile',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? 'ios-person' : 'md-person'}
+    />
+  )
+}
+
+ProfileStack.path = ''
+
+const tabNavigator = createBottomTabNavigator({
+  HomeStack,
+  ProfileStack
+})
+
+tabNavigator.path = ''
+
+export default tabNavigator
---
+++ mobile/repairit/screens/HomeScreen.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function HomeScreen () {
+  return (
+    <View style={styles.container}>
+      <Text>Home Screen</Text>
+    </View>
+  )
+}
+
+HomeScreen.navigationOptions = {
+  header: null
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff'
+  }
+})
